6d78feb5559a856dc0c96f6fea1911a318e24ce4
soumik12345 <19soumik.rakshit96@gmail.com>
2020-01-24 16:12:05 +0530

    Added Weight Decay

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from tensorflow.keras.optimizers.schedules import PolynomialDecay
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, LearningRateScheduler
 
 
-batch_size = 12
+batch_size = 4
 
 train_dataset = get_dataset(
     glob('./data/train_imgs/*'),
@@ -27,6 +27,9 @@ strategy = tf.distribute.MirroredStrategy()
 
 with strategy.scope():
     model = FastSCNN(n_classes=19)
+    for layer in model.layers:
+        if isinstance(layer, tf.keras.layers.Conv2D) or isinstance(layer, tf.keras.layers.Conv2DTranspose):
+            layer.add_loss(lambda: keras.regularizers.l2(1e-4)(layer.kernel))
     lr_scheduler = PolynomialDecay(5e-4, 1000, 1e-4, 0.9)
     optimizer = tf.keras.optimizers.SGD(momentum=0.9, learning_rate=lr_scheduler)
     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
