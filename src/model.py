bf53de4689069c94c52d4bd58f064bc9819da568
soumik12345 <19soumik.rakshit96@gmail.com>
2020-01-24 14:30:59 +0530

    Added ESNet Model

---
+++ src/model.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from .blocks import *
+
+
+def ESNet(input_shape=(1024, 512, 3)):
+    inp = tf.keras.layers.Input(input_shape)
+    ##### Encoder #####
+    # Block 1
+    x = DownsamplingBlock(inp, 3, 16)
+    x = FCU(x, 16, K=3)
+    x = FCU(x, 16, K=3)
+    x = FCU(x, 16, K=3)
+    # Block 2
+    x = DownsamplingBlock(x, 16, 64)
+    x = FCU(x, 64, K=5)
+    x = FCU(x, 64, K=5)
+    # Block 3
+    x = DownsamplingBlock(x, 64, 128)
+    x = PFCU(x, 128)
+    x = PFCU(x, 128)
+    x = PFCU(x, 128)
+    ##### Decoder #####
+    # Block 4
+    x = UpsamplingBlock(x, 64)
+    x = FCU(x, 64, K=5)
+    x = FCU(x, 64, K=5)
+    # Block 5
+    x = UpsamplingBlock(x, 16)
+    x = FCU(x, 16, K=3)
+    x = FCU(x, 16, K=3)
+    output = tf.keras.layers.Conv2DTranspose(
+        30, 3, padding='same',
+        strides=(2, 2), use_bias=True
+    )(x)
+    return tf.keras.models.Model(inp, output)
