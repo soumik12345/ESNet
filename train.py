c4e620ba89b19ff102fbde175320e4c2172bad3c
soumik12345 <19soumik.rakshit96@gmail.com>
2020-01-28 11:09:06 +0530

    Updated train.py for camvid

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from glob import glob
 from src.model import *
 from src.utils import *
 import tensorflow as tf
-from src.cityscapes import *
+from src.camvid import *
 from datetime import datetime
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.optimizers.schedules import PolynomialDecay
@@ -13,24 +13,24 @@ from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, LearningRat
 batch_size = 4
 
 train_dataset = get_dataset(
-    glob('./data/train_imgs/*'),
-    glob('./data/train_labels/*')
+    glob('./camvid/CamVid/train/*'),
+    glob('./camvid/CamVid/train_labels/*')
 )
 
 val_dataset = get_dataset(
-    glob('./data/val_imgs/*'),
-    glob('./data/val_labels/*')
+    glob('./camvid/CamVid/val/*'),
+    glob('./camvid/CamVid/val_labels/*')
 )
 
 loss = SparseCategoricalCrossentropy(from_logits=True)
 strategy = tf.distribute.MirroredStrategy()
 
 with strategy.scope():
-    model = FastSCNN(n_classes=19)
+    model = ESNet((512, 512, 3), 12)
     for layer in model.layers:
         if isinstance(layer, tf.keras.layers.Conv2D) or isinstance(layer, tf.keras.layers.Conv2DTranspose):
-            layer.add_loss(lambda: keras.regularizers.l2(1e-4)(layer.kernel))
-    lr_scheduler = PolynomialDecay(5e-4, 1000, 1e-4, 0.9)
+            layer.add_loss(lambda: tf.keras.regularizers.l2(1e-4)(layer.kernel))
+    lr_scheduler = tf.keras.optimizers.schedules.PolynomialDecay(5e-4, 1000, 1e-4, 0.9)
     optimizer = tf.keras.optimizers.SGD(momentum=0.9, learning_rate=lr_scheduler)
     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 
